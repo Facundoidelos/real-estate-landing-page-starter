971f7c99a62427f2942dd08d0c3b2d2026814edf
Facundoidelos <facundoidelos@gmail.com>
2022-10-19 12:24:11 -0300

    Completed

--- src/Components/GetStarted.jsx
+++ src/Components/GetStarted.jsx
@@ -5,11 +5,61 @@ import homeIllustration from "../media/illustration.png";
 import CustomButton from "./CustomButton";
 
 const GetStarted = () => {
-  const CustomContainer = styled(Container)(({ theme }) => ({}));
+  const CustomContainer = styled(Container)(({ theme }) => ({
+    backgroundColor: "#17275F",
+    height: "416px",
+    borderRadius: "15px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    [theme.breakpoints.down("md")]: {
+      height: "auto",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: theme.spacing(3, 3, 0, 3),
+      width: "90%",
+    },
+  }));
 
-  const CustomBox = styled(Box)(({ theme }) => ({}));
+  const CustomBox = styled(Box)(({ theme }) => ({
+    padding: theme.spacing(10, 0, 10, 0),
+    margin: theme.spacing(0, 2, 0, 2),
+    [theme.breakpoints.down("md")]: {
+      padding: "0",
+    },
+  }));
 
-  return <></>;
+  return (
+    <CustomBox>
+      <CustomContainer>
+        <Box>
+          <Typography
+            sx={{ fontSize: "35px", color: "white", fontWeight: "700" }}
+          >
+            Destacados
+          </Typography>
+          <Typography
+            sx={{ fontSize: "16px", color: "#ccc", fontWeight: "500", my: 3 }}
+          >
+           ¡Todo lo que necesitas saber acerca de las propiedades!
+          </Typography>
+
+          <CustomButton
+            backgroundColor="#fff"
+            color="#17275F"
+            buttonText="+Info"
+            getStartedBtn={true}
+          />
+        </Box>
+
+        <img
+          src={homeIllustration}
+          alt="illustration"
+          style={{ maxWidth: "100%" }}
+        />
+      </CustomContainer>
+    </CustomBox>
+  );
 };
 
 export default GetStarted;
--- src/Components/Properties.jsx
+++ src/Components/Properties.jsx
@@ -4,11 +4,52 @@ import House from "./House";
 import { properties } from "../properties";
 
 const Properties = () => {
-  const PropertiesBox = styled(Box)(({ theme }) => ({}));
+  const PropertiesBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: theme.spacing(5),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  }));
 
-  const PropertiesTextBox = styled(Box)(({ theme }) => ({}));
+  const PropertiesTextBox = styled(Box)(({ theme }) => ({
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+    },
+  }));
 
-  return <></>;
+  return (
+    <Box sx={{ mt: 5, backgroundColor: "#F5FAFE", py: 10 }}>
+      <Container>
+        <PropertiesTextBox>
+          <Typography
+            sx={{ color: "#000339", fontSize: "35px", fontWeight: "bold" }}
+          >
+            Propiedades destacadas
+          </Typography>
+          <Typography sx={{ color: "#5A6473", fontSize: "16px", mt: 1 }}>
+            ¡Todo lo mejor, está aqui!
+          </Typography>
+        </PropertiesTextBox>
+
+        <PropertiesBox>
+          {properties.map((property) => (
+            <House
+              key={property.id}
+              img={property.img}
+              price={property.price}
+              address={property.address}
+              bedrooms={property.bedrooms}
+              bathrooms={property.bathrooms}
+              space={property.space}
+            />
+          ))}
+        </PropertiesBox>
+      </Container>
+    </Box>
+  );
 };
 
 export default Properties;
